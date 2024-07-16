170b2cfb6420e7c55437623a42b7ac14c81ffebc
okara298 <shunokawara@gmail.com>
2024-07-17 01:24:01 +0900

    Update models.py

--- src/models.py
+++ src/models.py
@@ -10,30 +10,37 @@ class BasicConvClassifier(nn.Module):
         num_classes: int,
         seq_len: int,
         in_channels: int,
-        hid_dim: int = 128
+        hid_dim: int = 128,
+        num_subjects: int = 4
     ) -> None:
         super().__init__()
 
+        self.subject_embedding = nn.Embedding(num_subjects, hid_dim)  ##### 被験者IDを埋め込み層に変換
+
+        # 畳み込みブロックを定義
         self.blocks = nn.Sequential(
             ConvBlock(in_channels, hid_dim),
             ConvBlock(hid_dim, hid_dim),
             ConvBlock(hid_dim, hid_dim * 2),  ##### 追加の畳み込みブロック
         )
 
+        # ヘッドを定義
         self.head = nn.Sequential(
             nn.AdaptiveAvgPool1d(1),
             Rearrange("b d 1 -> b d"),
             nn.Linear(hid_dim * 2, num_classes),  ##### 隠れ層の次元を変更
         )
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X: torch.Tensor, subject_idxs: torch.Tensor) -> torch.Tensor:
         """_summary_
         Args:
-            X ( b, c, t ): _description_
+            X ( b, c, t ): _description_入力テンソル(バッチサイズ, チャネル数, 時間ステップ)
         Returns:
-            X ( b, num_classes ): _description_
+            X ( b, num_classes ): _description_出力テンソル(バッチサイズ, クラス数)
         """
         X = self.blocks(X)
+
+        X = X + subject_emb.unsqueeze(-1)  ##### ブロードキャストによる統合
 
         return self.head(X)
 
@@ -51,13 +58,16 @@ class ConvBlock(nn.Module):
         self.in_dim = in_dim
         self.out_dim = out_dim
 
+        # 畳み込み層を定義
         self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
         self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
-        # self.conv2 = nn.Conv1d(out_dim, out_dim, kernel_size) # , padding="same")
-        
+        self.conv2 = nn.Conv1d(out_dim, out_dim, kernel_size), padding="same")
+
+        # バッチ正則化層を定義
         self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
         self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
 
+        # ドロップアウト層を定義
         self.dropout = nn.Dropout(p_drop)
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
@@ -71,7 +81,7 @@ class ConvBlock(nn.Module):
         X = self.conv1(X) + X  # skip connection
         X = F.gelu(self.batchnorm1(X))
 
-        # X = self.conv2(X)
-        # X = F.glu(X, dim=-2)
+        X = self.conv2(X)
+        X = F.glu(X, dim=-2)
 
         return self.dropout(X)
