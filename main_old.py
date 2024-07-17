83d78738654324bf490907efd4a964a2d05da08f
okara298 <shunokawara@gmail.com>
2024-07-17 13:00:32 +0900

    Create main_old.py

---
+++ main_old.py
@@ -0,0 +1,123 @@
+import os, sys
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torchmetrics import Accuracy
+import hydra
+from omegaconf import DictConfig
+import wandb
+from termcolor import cprint
+from tqdm import tqdm
+
+from src.datasets import ThingsMEGDataset
+from src.models import BasicConvClassifier
+from src.utils import set_seed
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def run(args: DictConfig):
+    set_seed(args.seed)
+    logdir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    
+    if args.use_wandb:
+        wandb.init(mode="online", dir=logdir, project="MEG-classification")
+
+    # ------------------
+    #    Dataloader
+    # ------------------
+    loader_args = {"batch_size": args.batch_size, "num_workers": args.num_workers}
+    
+    train_set = ThingsMEGDataset("train", args.data_dir)
+    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
+    val_set = ThingsMEGDataset("val", args.data_dir)
+    val_loader = torch.utils.data.DataLoader(val_set, shuffle=False, **loader_args)
+    test_set = ThingsMEGDataset("test", args.data_dir)
+    test_loader = torch.utils.data.DataLoader(
+        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
+    )
+
+    # ------------------
+    #       Model　　　　　　　　　　被験者番号 num_subjectsの追加
+    # ------------------
+    model = BasicConvClassifier(
+        train_set.num_classes, train_set.seq_len, train_set.num_channels, num_subjects=4
+    ).to(args.device)
+
+    # ------------------
+    #     Optimizer
+    # ------------------
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    ##### 学習率スケジューラの追加 #####
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.5)
+
+    # ------------------
+    #   Start training
+    # ------------------  
+    max_val_acc = 0
+    accuracy = Accuracy(
+        task="multiclass", num_classes=train_set.num_classes, top_k=10
+    ).to(args.device)
+
+      
+    for epoch in range(args.epochs):
+        print(f"Epoch {epoch+1}/{args.epochs}")
+        
+        train_loss, train_acc, val_loss, val_acc = [], [], [], []
+        
+        model.train()
+        for X, y, subject_idxs in tqdm(train_loader, desc="Train"):
+            X, y, subject_idxs = X.to(args.device), y.to(args.device), subject_idxs.to(args.device)  # 被験者番号の追加
+
+            y_pred = model(X, subject_idxs)  # 被験者番号の追加
+            
+            loss = F.cross_entropy(y_pred, y)
+            train_loss.append(loss.item())
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            acc = accuracy(y_pred, y)
+            train_acc.append(acc.item())
+
+        model.eval()
+        for X, y, subject_idxs in tqdm(val_loader, desc="Validation"):
+            X, y, subject_idxs = X.to(args.device), y.to(args.device), subject_idxs.to(args.device)  # 被験者番号の追加
+            
+            with torch.no_grad():
+                y_pred = model(X, subject_idxs)  # 被験者番号の追加
+            
+            val_loss.append(F.cross_entropy(y_pred, y).item())
+            val_acc.append(accuracy(y_pred, y).item())
+
+        print(f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train acc: {np.mean(train_acc):.3f} | val loss: {np.mean(val_loss):.3f} | val acc: {np.mean(val_acc):.3f}")
+        torch.save(model.state_dict(), os.path.join(logdir, "model_last.pt"))
+        if args.use_wandb:
+            wandb.log({"train_loss": np.mean(train_loss), "train_acc": np.mean(train_acc), "val_loss": np.mean(val_loss), "val_acc": np.mean(val_acc)})
+        
+        if np.mean(val_acc) > max_val_acc:
+            cprint("New best.", "cyan")
+            torch.save(model.state_dict(), os.path.join(logdir, "model_best.pt"))
+            max_val_acc = np.mean(val_acc)
+
+        ##### バリデーションの後に学習率スケジューラをステップさせる #####
+        scheduler.step(np.mean(val_loss))
+            
+    
+    # ----------------------------------
+    #  Start evaluation with best model
+    # ----------------------------------
+    model.load_state_dict(torch.load(os.path.join(logdir, "model_best.pt"), map_location=args.device))
+
+    preds = [] 
+    model.eval()
+    for X, subject_idxs in tqdm(test_loader, desc="Validation"):        
+        preds.append(model(X.to(args.device), subject_idxs.to(args.device)).detach().cpu())  # 被験者番号の追加
+        
+    preds = torch.cat(preds, dim=0).numpy()
+    np.save(os.path.join(logdir, "submission"), preds)
+    cprint(f"Submission {preds.shape} saved at {logdir}", "cyan")
+
+
+if __name__ == "__main__":
+    run()
